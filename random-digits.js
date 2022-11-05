0bd787b6e00297ca198f1906f1c0e8b74d51f51e
OlgaBuLL <108359930+OlgaBuLL@users.noreply.github.com>
2022-11-05 18:13:29 +0100

    first commit

---
+++ random-digits.js
@@ -0,0 +1,54 @@
+// function generateNumbers(min, max) {
+//   // min = Math.ceil(-10);
+//   // max = Math.floor(10);
+//   //
+//   //   numbersArray.push(Math.round(Math.random() * 10));
+//   return Math.round(Math.random() * (max - min) + min);
+
+//   //   numbersArray.push(Math.round(Math.random() * -10));
+//   //     numbersArray.push(Math.round(Math.random() * 10));
+//   //     numbersArray.push(Math.round(Math.random() * -10));
+
+//   //   document.getElementById("generated").innerHTML = numbersArray;
+//   //   console.log(numbersArray);
+// }
+function generateNumbers() {
+  let min = -10;
+  let max = 10;
+  let minimalNumber = 0;
+  let maximalNumber = 0;
+  let summ = 0;
+  let multiply = 1;
+  let average = 0;
+  let numbersArray = [];
+  for (let i = 0; i < 10; i++) {
+    numbersArray[i] = Math.round(Math.random() * (max - min) + min);
+    summ += numbersArray[i];
+    multiply *= numbersArray[i];
+
+    minimalNumber = Math.min.apply(null, numbersArray);
+    maximalNumber = Math.max.apply(null, numbersArray);
+  }
+
+  console.log("Сгенерированный массив: " + numbersArray);
+  document.getElementById("generated").innerHTML =
+    "Generated array : " + numbersArray;
+
+  console.log("Минимальное значение: " + minimalNumber);
+  document.getElementById("minimal").innerHTML =
+    "Minimal number: " + minimalNumber;
+
+  console.log("Максимальное значение: " + maximalNumber);
+  document.getElementById("maximal").innerHTML =
+    "Maximal number: " + maximalNumber;
+
+  average = summ / 10;
+  console.log("Среднее арифметическое: " + average);
+  document.getElementById("average").innerHTML = "Average: " + average;
+
+  console.log("Сумма чисел массива: " + summ);
+  document.getElementById("summ").innerHTML = "Sum: " + summ;
+
+  console.log("Произведение чисел массива: " + multiply);
+  document.getElementById("product").innerHTML = "Multiplication: " + multiply;
+}
